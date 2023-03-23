6dd92cd2e70712b9c8996468da01d36c19ef069f
Gene <jsiow002.dev@gmail.com>
2023-03-23 08:42:10 -0700

    49parttwo

--- Round_Two/49.py
+++ Round_Two/49.py
@@ -8,3 +8,17 @@ class Solution:
             ht[''.join(sorted(string))].append(string) 
         
         return ht.values()
+"""
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ht = defaultdict(list)
+        for string in strs:
+            count = [0] * 26
+            for s in string:
+                count[ord(s) - ord('a')] += 1
+            ht[tuple(count)].append(string)
+        return ht.values()
+
+"""
