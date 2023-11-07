4f09e39867cf8f47312f217a581afb6082e42eb8
Gene <jsiow002.dev@gmail.com>
2023-11-07 00:10:20 -0800

    27

---
+++ Round_Two/27.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        l = 0
+        r = len(nums)
+        while l < r:
+            if nums[l] == val:
+                nums[l]= nums[r-1]
+                r -=1
+            else:
+                l +=1
+        print(nums)
+        print(r)
+        return r
