91c93448f2ba0dcbabf10ad521623509232357de
Gene <jsiow002.dev@gmail.com>
2024-06-07 16:10:30 -0700

    509

---
+++ Round_Two/509.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def __init__(self):
+        self.cache = dict()
+        self.cache[0] = 0
+        self.cache[1] = 1
+
+    def fib(self, n: int) -> int:
+        if n in self.cache:
+            return self.cache[n]
+        self.cache[n] = self.fib(n-1) + self.fib(n-2)
+        return self.cache[n]
